40a99ce19a99df3b416719bc529d9f08b1cbbea2
Flanamo <ronheutinck@gmail.com>
2020-04-08 00:19:50 +0200

    fuck you snek

---
+++ commands/list.js
@@ -0,0 +1,14 @@
+// const { list } = require('../functions.js');
+// const characters = require('../characters.json');
+// const { MessageEmbed } = require('discord.js');
+//
+// module.exports = {
+//
+//     name: 'list',
+//     description: 'A list of all characters',
+//     execute(message, args) {
+//
+//
+//     }
+// };
+//
